26cff036bc14c934fb5002ba794859ef77a6631c
akhil biswas <akhil.biswas@isentia.com>
2021-03-31 07:51:57 +1100

    clean code

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,17 +1,18 @@
 const { httpGet } = require('./mock-http-interface');
+
 const getArnieQuotes = async (urls) => {
   const requests = urls.map((url) => {
-    return httpGet(url) 
-     .then((response) => {
-      if (response.status === 200) {
-        result = { 'Arnie Quote': JSON.parse(response.body).message };
-      } else {
-        result = { 'FAILURE': JSON.parse(response.body).message };
-      }
-      return result;
+    return httpGet(url)
+      .then((response) => {
+        if (response.status === 200) {
+          result = { 'Arnie Quote': JSON.parse(response.body).message };
+        } else {
+          result = { 'FAILURE': JSON.parse(response.body).message };
+        }
+        return result;
       })
   })
-  return Promise.all(requests) 
+  return Promise.all(requests)
 }
 
 module.exports = {
